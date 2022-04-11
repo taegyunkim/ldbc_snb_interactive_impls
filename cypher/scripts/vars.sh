765d398b837bdfc655da659e66e0fec4b9806063
Taegyun Kim <k.taegyun@gmail.com>
2022-04-10 22:08:38 -0400

    query logging options

--- cypher/scripts/vars.sh
+++ cypher/scripts/vars.sh
@@ -5,8 +5,13 @@ export NEO4J_CONTAINER_NAME=snb-interactive-neo4j
 export NEO4J_CONTAINER_ROOT=`pwd`/scratch
 export NEO4J_CSV_POSTFIX=_0_0.csv
 export NEO4J_DATA_DIR=${NEO4J_CONTAINER_ROOT}/data
-export NEO4J_ENV_VARS=""
-export NEO4J_VERSION=4.4.1
+export NEO4J_ENV_VARS="--env=NEO4J_dbms_logs_default__format=JSON \
+  --env=NEO4J_dbms_logs_query_time__logging__enabled=true \
+  --env=NEO4J_dbms_track__query__cpu__time=true \
+  --env=NEO4J_dbms_logs_query_allocation__logging__enabled=true \
+  --env=NEO4J_dbms_logs_query_page__logging__enabled=true \
+  --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes"
+export NEO4J_VERSION=4.4.1-enterprise
 
 if [[ `uname -m` == "arm64" ]]; then
     export NEO4J_DOCKER_PLATFORM_FLAG="--platform linux/arm64"
